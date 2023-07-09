f6c85fb04bccbc0ab45e50e2334c3adc75e7a10a
Hoon Wee <mrgravity817@gmail.com>
2023-07-09 22:49:13 +0900

    feat: add function that has 3s delay

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,18 +1,8 @@
 defmodule Sender do
-  @moduledoc """
-  Documentation for `Sender`.
-  """
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Sender.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    {:ok, "email_sent"}
   end
 end
